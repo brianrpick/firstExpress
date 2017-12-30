d26e925a14887f28538cb1c677b00fa3c963b12d
Brian Pick <brianr.pick@gmail.com>
2017-12-29 19:34:31 -0800

    Added another route

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get("/", function(req, res){
 	res.send("hi there");
 });
 
+app.get("/bye", function(req, res){
+	res.send ("Goodbye");
+});
+
 app.listen(3000, function(){
 	console.log("serving app on port 3000")
 })
