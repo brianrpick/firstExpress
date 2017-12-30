7c43cfd69212eed8a0930f6b77070447966b2675
Brian Pick <brianr.pick@gmail.com>
2017-12-29 19:50:59 -0800

    Added * route

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get("/bye", function(req, res){
 	res.send ("Goodbye");
 });
 
+app.get("*", function(req, res){
+	res.send("you are a star");
+});
+
 app.listen(3000, function(){
 	console.log("serving app on port 3000")
 })
