fbc32009ec91123ae7966d7db3fa81ec1895c024
Brian Pick <brianr.pick@gmail.com>
2017-12-31 12:37:12 -0800

    playing with dynamic routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 
+
 app.get("/", function(req, res){
 	res.send("hi there");
 });
@@ -9,10 +10,20 @@ app.get("/bye", function(req, res){
 	res.send ("Goodbye");
 });
 
+app.get("/r/:subredditName", function(req, res){
+	res.send("Welcome to a subreddit");
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res){
+	var subReddit = req.params.subredditName;
+	res.send("Welcome to the " + subReddit + " page");
+});
+
+
 app.get("*", function(req, res){
 	res.send("you are a star");
 });
 
 app.listen(3000, function(){
 	console.log("serving app on port 3000")
-})
+})  
